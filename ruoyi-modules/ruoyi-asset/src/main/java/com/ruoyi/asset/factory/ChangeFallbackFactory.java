f3187b95ecae1c613f8767f20455744837015118
WangQin <2708077252@qq.com>
2026-07-26 15:10:26 +0800

    fix: 面试篇增加了很多对Java的了解，于是回来对源码做了一些稍微修复修改

--- ruoyi-modules/ruoyi-asset/src/main/java/com/ruoyi/asset/factory/ChangeFallbackFactory.java
+++ ruoyi-modules/ruoyi-asset/src/main/java/com/ruoyi/asset/factory/ChangeFallbackFactory.java
@@ -5,6 +5,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FallbackFactory;
 
+/**
+ * @author FATHER
+ */
 public class ChangeFallbackFactory implements FallbackFactory<IChangeService> {
     private static final Logger log = LoggerFactory.getLogger(ChangeFallbackFactory.class);
     @Override
